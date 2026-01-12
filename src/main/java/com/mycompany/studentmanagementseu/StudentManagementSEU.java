c5b9650d602f80794d44ee670a8440a4c97cb73b
haisibul2hasan <hasanshantoemail@gmail.com>
2026-01-13 01:47:50 +0600

    First

---
+++ src/main/java/com/mycompany/studentmanagementseu/StudentManagementSEU.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.studentmanagementseu;
+
+
+public class StudentManagementSEU {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
